0f22e49859a4433262f07307c703795be0e3b553
ashurovferuz81-ui <ashurovferuz81@gmail.com>
2026-02-07 18:13:22 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+ADMIN_ID = 5775388579
+TOKEN = "8251777312:AAGdnZKgyB2CSEOJPrNaGTCShSf5FeWDbDA"
+DATABASE_URL = "postgres://username:password@host:port/dbname"
+REDIS_URL = "redis://default:password@host:port"
+CHANNEL = "@kanalingiz"
